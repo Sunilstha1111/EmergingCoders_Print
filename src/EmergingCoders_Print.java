1f9eaf3816450170ecf7f7346cf3e5335cc79d5c
Sunil Shrestha <sunilstha108@gmail.com>
2017-10-02 16:36:13 -0400

    Initial commit

---
+++ src/EmergingCoders_Print.java
@@ -0,0 +1,7 @@
+public class EmergingCoders_Print {
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Today is October 2, 2017. I am here at Howard University.");
+        
+    }
+}
